976694120d485da9f6a0d53506a4c56fe30fd471
Saamin Cheema <sc46@sanger.ac.uk>
2024-07-06 00:28:57 +0100

    Separate LFC calculation

---
+++ scripts/calculate_lfc.R
@@ -0,0 +1,88 @@
+library(dplyr)
+library(readr)
+
+# Load depmap data
+avana_sk_ranks <- read_tsv("processed_data/avana_sk_ranks.tsv")
+avana_Nsk_ranks <- read_tsv("processed_data/avana_Nsk_ranks.tsv")
+
+# Load UVM data
+uvm_ranks <- read_tsv("processed_data/uvm_ranks.tsv")
+
+# Load pan essential genes list
+pan_essential_genes <- read_csv("raw_data/pan_genes.csv")
+
+# Perform wilcox test
+perform_mann_whitney <- function(data_a, data_b) {
+  test_result <- wilcox.test(as.numeric(data_a), as.numeric(data_b))
+  return(c(
+    W_statistic = as.numeric(test_result[["statistic"]]),
+    p_value = test_result[["p.value"]]
+  ))
+}
+
+get_mann_whitney_results <- function(ranks_df, ranks_df_2) {
+  df <- t(apply(cbind(ranks_df[, -1], ranks_df_2[, -1]), 1, function(row) {
+    perform_mann_whitney(
+      row[1:ncol(ranks_df) - 1], row[(ncol(ranks_df)):length(row)]
+    )
+  }))
+
+  df <- as.data.frame(df) %>%
+    mutate(genes =  ranks_df[["genes"]]) %>%
+    mutate(Padj = p.adjust(p_value, method = "BH")) %>%
+    select(genes, W_statistic, p_value, Padj)
+
+  return(df)
+}
+
+# UVM vs SKCM
+uvm_vs_skcm <- get_mann_whitney_results(ranked_uvm, ranked_avana_sk)
+
+# UVM vs Pan Cancer
+uvm_vs_pan_cancer <- get_mann_whitney_results(ranked_uvm, ranked_avana_Nsk)
+
+# 'LFC'...?
+get_median_ranks <- fuction() {
+
+}
+uvm_medians <- apply(ranked_uvm[, -1], 1, median)
+sk_medians <- apply(ranked_avana_sk[, -1], 1, median)
+nsk_medians <- apply(ranked_avana_nsk[, -1], 1, median)
+
+uvm_vs_skcm$uvm_median_rank <- uvm_medians
+uvm_vs_skcm$sk_median_rank <- sk_medians
+uvm_vs_skcm$FC <- uvm_vs_skcm$sk_median_rank / uvm_vs_skcm$uvm_median_rank
+uvm_vs_skcm$LFC <- log2(uvm_vs_skcm$FC)
+
+uvm_vs_pan_cancer$uvm_median_rank <- uvm_medians
+uvm_vs_pan_cancer$nsk_median_rank <- nsk_medians
+uvm_vs_pan_cancer$FC <- uvm_vs_pan_cancer$nsk_median_rank / uvm_vs_pan_cancer$uvm_median_rank
+uvm_vs_pan_cancer$LFC <- log2(uvm_vs_skcm$FC)
+
+# Combine data
+uvm_vs_skcm <- merge(uvm_vs_skcm, merged_ess[, c("genes", "UVM", "SKCM")],
+  by = "genes"
+)
+uvm_vs_pan_cancer <- merge(uvm_vs_pan_cancer, merged_ess[, c("genes", "UVM", "Pan_cancer")],
+  by = "genes"
+)
+
+# Pan ess genes
+Log2FC_cutoff <- 1.5
+padj_cutoff <- 0.05
+
+uvm_vs_skcm_filtered <- uvm_vs_skcm |>
+  filter(!genes %in% pan_essential_genes[["Gene"]])
+
+uvm_vs_skcm_filtered_sig <- uvm_vs_skcm_filtered |>
+  filter(LFC > Log2FC_cutoff) |>
+  filter(Padj < padj_cutoff) |>
+  arrange(desc(LFC))
+
+uvm_vs_pan_cancer_filtered <- uvm_vs_pan_cancer |>
+  filter(!genes %in% pan_essential_genes[["Gene"]])
+
+uvm_vs_pan_cancer_filtered_sig <- uvm_vs_pan_cancer_filtered |>
+  filter(LFC > Log2FC_cutoff) |>
+  filter(Padj < padj_cutoff) |>
+  arrange(desc(LFC))
