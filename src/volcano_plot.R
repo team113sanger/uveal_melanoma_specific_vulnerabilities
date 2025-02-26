2a79b0b07e840105a22c6659ec385b77340389ee
Saamin Cheema <sc46@sanger.ac.uk>
2025-02-26 20:18:27 +0000

    Change significance threshold and aethetics to match Dave's

--- src/volcano_plot.R
+++ src/volcano_plot.R
@@ -3,43 +3,42 @@ library(ggplot2)
 library(ggrepel)
 
 label_significant_genes <- function(df) {
-  plot_data <- df %>%
+  df %>%
     mutate(
       significant = case_when(
-        Padj < 0.05 & abs(LFC) > 2 ~
-          ifelse(LFC > 0, "Upregulated", "Downregulated"),
+        Padj < 0.05 & abs(log2FC) > 1.8 ~ ifelse(log2FC > 0, "Upregulated", "Downregulated"),
         TRUE ~ "Not Significant"
       )
     )
 }
-
 plot_volcano <- function(df) {
   top_genes <- df %>%
     filter(significant != "Not Significant") %>%
-    arrange(desc(abs(LFC))) %>%
-    head(10, abs(LFC))
-
-  ggplot(df, aes(x = LFC, y = -log10(Padj), color = significant)) +
-    geom_point(alpha = 0.7) +
-    scale_color_manual(
-      values = c(
-        "Not Significant" = "gray", "Upregulated" = "indianred3",
-        "Downregulated" = "royalblue3"
-      ),
-      guide = "none"
-    ) +
-    geom_hline(
-      yintercept = -log10(0.05), linetype = "dashed",
-      color = "black", alpha = 0.5
-    ) +
-    geom_vline(
-      xintercept = c(-2, 2), linetype = "dashed",
-      color = "black", alpha = 0.5
-    ) +
-    geom_text_repel(
-      data = top_genes, aes(label = genes), size = 3,
-      box.padding = unit(0.35, "lines"), point.padding = unit(0.3, "lines")
-    ) +
-    labs(x = "Log2(Fold Change)", y = "-Log10(Padj)") +
-    theme_bw()
+    arrange(desc(log2FC)) %>%
+    head(10)
+    
+  ggplot(df, aes(x = log2FC, y = -log10(Padj), color = significant)) +
+      geom_point(shape = 19, alpha = 0.7) +
+      scale_color_manual(
+        values = c("Not Significant" = "#808080", "Upregulated" = "#226E9C", "Downregulated" = "#2E8B57")
+      ) +
+      geom_vline(xintercept = c(-1.8, 1.8), linetype = "dashed", color = "black", alpha = 0.5) +
+      # geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "black", alpha = 0.5) +
+      geom_text_repel(
+        data = top_genes, aes(label = genes),
+        color = "black", fontface = "italic", size = 4,
+        min.segment.length = 0
+      ) +
+      labs(
+        x = expression('log'[2]~'(Fold change)'),
+        y = expression('log'[10]~'(1 / Padj)')
+      ) +
+      theme_classic() +
+      theme(
+        axis.title.x = element_text(size = 15),
+        axis.title.y = element_text(size = 15),
+        axis.text = element_text(size = 13, color = "black"),
+        legend.position = "none"  
+      ) +
+      scale_x_reverse() 
 }
